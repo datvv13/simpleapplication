df2c9fef2824d10399c276359656b6a75f1a31df
datvv13 <15520111@gm.uit.edu.vn>
2019-02-22 15:32:18 +0700

    Create BasicapplicationApplication.java

---
+++ basicapplication/src/main/java/simpleapplication/BasicapplicationApplication.java
@@ -0,0 +1,15 @@
+package simpleapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+
+
+@SpringBootApplication
+public class BasicapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicapplicationApplication.class, args);
+	}
+
+}
